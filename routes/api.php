47bcfe52718c2af97da37eaaa3d1d250ff5c3642
ekawahyubudisantoso <ekawahyubudisantoso22@gmail.com>
2025-05-23 11:12:29 +0700

    Menambahkan API Authentication (Login, Logout)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+
 });
+
+Route::post('/login', [AuthController::class, 'login']);
